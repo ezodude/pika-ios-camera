baf18a3a6a7fba69363ce2a99ba4e1790c6b2235
Ezo Saleh <ezo@the-experimenters.com>
2017-07-04 17:29:17 +0100

    Added camera button.

--- PikaCamera/CameraViewController.swift
+++ PikaCamera/CameraViewController.swift
@@ -7,14 +7,17 @@
 //
 
 import UIKit
+import QuartzCore
 
 class CameraViewController: UIViewController {
   
   @IBOutlet weak var cameraLabel: UILabel!
+  @IBOutlet weak var photoButton: UIButton!
   
   override func viewDidLoad() {
     super.viewDidLoad()
     cameraLabel.text = "CAMERA ON"
+    photoButton.layer.borderColor = UIColor.yellow.cgColor
   }
   
 }
