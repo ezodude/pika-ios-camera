c6a714eb8e443ff61a9c3c7096dc7a408ea84b81
Ezo Saleh <ezo@the-experimenters.com>
2017-08-01 13:43:00 +0100

    Removed unnecessary vimage wrappers.

--- PikaCamera/Utils/PCVImageWrapper.m
+++
@@ -1,9 +0,0 @@
-//
-//  PCVImageWrapper.m
-//  PikaCamera
-//
-//  Created by Ezo Saleh on 01/08/2017.
-//  Copyright © 2017 Pika Vision. All rights reserved.
-//
-
-#import <Foundation/Foundation.h>
