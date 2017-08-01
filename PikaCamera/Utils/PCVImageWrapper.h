adddaf3ff5855ee671f9da7d3a8bd3e1e560dc9c
Ezo Saleh <ezo@the-experimenters.com>
2017-08-01 11:45:52 +0100

    Added the accelerate framework.

---
+++ PikaCamera/Utils/PCVImageWrapper.h
@@ -0,0 +1,13 @@
+//
+//  PCVImageWrapper.h
+//  PikaCamera
+//
+//  Created by Ezo Saleh on 01/08/2017.
+//  Copyright © 2017 Pika Vision. All rights reserved.
+//
+
+#ifndef PCVImageWrapper_h
+#define PCVImageWrapper_h
+
+
+#endif /* PCVImageWrapper_h */
